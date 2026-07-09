b73c46c78e922389d4bdaf9912df04ee62533ce8
vijayksingh <vijayksingh@users.noreply.github.com>
2026-07-09 17:43:11 +0530

    Add CI publishing for TIL Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://vijayksingh.github.io",
-  base: process.env.GITHUB_PAGES === "true" ? "/DS-Notes" : "/",
+  base: process.env.GITHUB_PAGES === "true" ? "/TIL" : "/",
   output: "static",
 });
